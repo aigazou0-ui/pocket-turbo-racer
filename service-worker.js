d33293f32a531d0bcadfa507b04d2d5f15caa0dd
aigazou0-ui <aigazou0@gmail.com>
2026-06-04 20:05:15 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pocket-turbo-racer-v18-cpu-rank-balance";
+const CACHE_NAME = "pocket-turbo-racer-v18m-all-courses-final-line";
 const CORE_ASSETS = [
   "./",
   "./index.html",
