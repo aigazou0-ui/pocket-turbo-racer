e1c1b99ff42872fc495af477c9ffd9b696ecc90d
eiyo777-svg <aigazou0@gmail.com>
2026-06-06 14:27:52 +0900

    v18ac restore yesterday title

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pocket-turbo-racer-v18aa-title-restored";
+const CACHE_NAME = "pocket-turbo-racer-v18ac-title-restored-yesterday";
 const CORE_ASSETS = [
   "./",
   "./index.html",
