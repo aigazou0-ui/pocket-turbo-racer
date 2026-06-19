b514896b5d3ca062d67d5cca540b67cb5b4d8f7f
eiyo777-svg <aigazou0@gmail.com>
2026-06-19 20:57:05 +0900

    v18bb adjust turbo HUD width

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pocket-turbo-racer-v18ba-force-one-line-android-hud";
+const CACHE_NAME = "pocket-turbo-racer-v18bb-adjust-turbo-hud-width";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self["add" + "EventListener"]("install", event => {
